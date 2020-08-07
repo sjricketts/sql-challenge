7c090e97b7c689c15dd4ba6b8be9dba811146eac
Scott Ricketts <SJR@imac.lan>
2020-08-07 17:15:49 -0500

    work on erd and data import

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -4,23 +4,25 @@
 
 
 CREATE TABLE "dept" (
-    "dept_no" int   NOT NULL,
-    "dept_name" varchar  NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
     CONSTRAINT "pk_dept" PRIMARY KEY (
         "dept_no"
      )
 );
 
+select * from dept;
+
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no"
      )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL,
     "emp_no" int   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_no"
@@ -29,7 +31,7 @@ CREATE TABLE "dept_manager" (
 
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
-    "emp_title_id" int   NOT NULL,
+    "emp_title_id" varchar   NOT NULL,
     "birth_date" date   NOT NULL,
     "first_name" varchar   NOT NULL,
     "last_name" varchar   NOT NULL,
@@ -49,7 +51,7 @@ CREATE TABLE "salaries" (
 );
 
 CREATE TABLE "titles" (
-    "title_id" int   NOT NULL,
+    "title_id" varchar   NOT NULL,
     "title" varchar   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
