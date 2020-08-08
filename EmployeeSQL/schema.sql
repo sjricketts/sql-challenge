266d015ca763f95194ba2eeae81bfbe23b0e3173
Scott Ricketts <SJR@imac.lan>
2020-08-08 08:01:42 -0500

    finish proper schema and import csv data

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,34 +1,47 @@
+
 CREATE TABLE employees (
-    emp_no int  Primary Key NOT NULL,
-    emp_title_id varchar   NOT NULL,
-    birth_date date   NOT NULL,
-    first_name varchar   NOT NULL,
-    last_name varchar   NOT NULL,
-    sex varchar   NOT NULL,
-    hire_date date   NOT NULL
+    emp_no int primary key,
+    emp_title_id varchar NOT NULL,
+    birth_date date NOT NULL,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    sex varchar NOT NULL,
+    hire_date date NOT NULL
 );
+
+select * from employees;
 
 CREATE TABLE dept_emp (
-    dept_no varchar   NOT NULL,
-    emp_no int   NOT NULL
+    emp_no int NOT NULL,
+    dept_no varchar NOT NULL
 );
+
+select * from dept_emp;
 
 CREATE TABLE salaries (
-    salary int   NOT NULL,
-    emp_no int   NOT NULL
+    emp_no int NOT NULL,
+    salary int NOT NULL
 );
+
+select * from salaries;
 
 CREATE TABLE departments (
-    dept_name varchar   NOT NULL,
-    dept_no varchar   NOT NULL
+    dept_no varchar NOT NULL,
+    dept_name varchar NOT NULL
 );
+
+select * from departments;
 
 CREATE TABLE dept_manager (
-    dept_no varchar   NOT NULL,
-    emp_no int   NOT NULL
+    dept_no varchar NOT NULL,
+    emp_no int NOT NULL
 );
 
+select * from dept_manager;
+
 CREATE TABLE titles (
-    title_id varchar   NOT NULL,
-    title varchar   NOT NULL
+    title_id varchar NOT NULL,
+    title varchar NOT NULL
 );
+
+select * from titles;
