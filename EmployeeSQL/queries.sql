cacf32368f7f173a40fbb07eb52d721381a7f354
Scott Ricketts <SJR@imac.lan>
2020-08-08 08:28:39 -0500

    complete data analysis 1 and 2.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,9 +1,15 @@
 -- 1. List the following details of each employee:
 --		employee number, last name, first name, sex, and salary.
-
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary 
+from employees as e
+inner join salaries as s on
+e.emp_no = s.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
-
+select first_name, last_name, hire_date 
+from employees 
+where hire_date BETWEEN '1986-01-01' and  '1986-12-31'
+order by hire_date;
 
 -- 3. List the manager of each department with the following information:
 -- 		department number, department name, the manager's employee number, last name, first name.
