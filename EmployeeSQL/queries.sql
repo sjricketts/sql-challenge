43fcf2449d05ad2d526f90c9908193a40f1c41f2
Scott Ricketts <SJR@imac.lan>
2020-08-08 09:01:16 -0500

    complete data analysis 6, 7, 8.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -39,11 +39,27 @@ where first_name = 'Hercules' and last_name like 'B%';
 
 -- 6. List all employees in the Sales department,
 --		including their employee number, last name, first name, and department name.
-
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+inner join dept_emp on
+e.emp_no = dept_emp.emp_no
+inner join departments as d on
+dept_emp.dept_no = d.dept_no
+where d.dept_name = 'Sales';
 
 -- 7. List all employees in the Sales and Development departments,
 --		including their employee number, last name, first name, and department name.
-
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+inner join dept_emp on
+e.emp_no = dept_emp.emp_no
+inner join departments as d on
+dept_emp.dept_no = d.dept_no
+where d.dept_name = 'Sales' or d.dept_name = 'Development';
 
 -- 8. In descending order, list the frequency count of employee last names,
 --		i.e., how many employees share each last name.
+select count(last_name) as "Count of Last Name", last_name as "Last Name"
+from employees
+group by last_name
+order by count(last_name) desc;
